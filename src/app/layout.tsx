9833b7af5090df3274d0529b66024e3a4357195f
Alexander Kurganov <a.kurganow@gmail.com>
2023-12-05 00:17:26 +0400

    feat: change favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,13 +17,13 @@ export default function RootLayout({
 	return (
 		<html lang="en" data-color-mode="light">
 			<head>
-				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-				<link rel="manifest" href="/site.webmanifest" />
-				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-				<meta name="msapplication-TileColor" content="#ffffff" />
-				<meta name="theme-color" content="#ffffff" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+				<link rel="manifest" href="/site.webmanifest"/>
+				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#663399"/>
+				<meta name="msapplication-TileColor" content="#663399"/>
+				<meta name="theme-color" content="#ffffff"/>
 			</head>
 			<body className={inter.className}>
 				{children}
