832137fcfa3d763e3110e2b6042317eb3381cc56
Alexander Kurganov <a.kurganow@gmail.com>
2023-12-05 16:30:05 +0400

    chore: rename

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -2,7 +2,7 @@ import st from './styles.module.css'
 import Image from 'next/image'
 
 export default function Logo() {
-	return <div className={st.logo} title="nanoits">
+	return <div className={st.logo} title="Alexenda">
 		<Image src="/logo.png" alt="Alexenda" width={48} height={48} />
 	</div>
 }
