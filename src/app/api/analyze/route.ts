a8ca4e5d726a7a44d7b3c9ef6b274209dfe87758
Alexander Kurganov <a.kurganow@gmail.com>
2023-12-08 23:06:42 +0400

    chore: refactoring

--- src/app/api/analyze/route.ts
+++ src/app/api/analyze/route.ts
@@ -13,22 +13,17 @@ const openai = new OpenAI({
 export async function POST(req: NextRequest) {
 	const { tasks, tags, locale, userId } = await req.json()
 
+	const systemMessageContent: string = systemMessage[locale.split(/[-_]/i)[0]] || systemMessage.en
+	const tasksMessageContent: string = tasks ? `Here the serialized into csv list of tasks ${json2csv(tasks)}` : 'I have no tasks yet'
+	const tagsMessageContent: string = tags ? `Here the serialized into csv list of tags ${json2csv(tags)}` : 'I have no tags yet'
+
 	const response = await openai.chat.completions.create({
 		stream: true,
-		model: 'gpt-4-1106-preview',
+		model: 'gpt-4',
 		messages: [
-			{
-				role: 'system',
-				content: (systemMessage[locale.split(/[-_]/i)[0]] || systemMessage.en),
-			},
-			{
-				role: 'user',
-				content: `Here the serialized into csv list of tasks ${json2csv(tasks)}`,
-			},
-			{
-				role: 'user',
-				content: `Here the serialized into csv list of tags ${json2csv(tags)}`,
-			}
+			{ role: 'system', content: systemMessageContent },
+			{ role: 'user', content: tasksMessageContent },
+			{ role: 'user', content: tagsMessageContent },
 		],
 		user: userId as string,
 	})
