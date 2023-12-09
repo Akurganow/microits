dc518b9d3f622dac6e0a27d9cf31b21a6a94fe85
Alexander Kurganov <a.kurganow@gmail.com>
2023-12-09 06:41:03 +0400

    feat: add Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from '@vercel/analytics/react'
 import 'app/globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -30,6 +31,7 @@ export default function RootLayout({
 				{children}
 				<div id="dialog"></div>
 				<SpeedInsights/>
+				<Analytics />
 			</body>
 		</html>
 	)
