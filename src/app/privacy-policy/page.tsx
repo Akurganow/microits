57ed5619fb6cc4c4527338c9e9c9efb44610dd34
Alexander Kurganov <a.kurganow@gmail.com>
2023-12-11 22:20:45 +0400

    fix: update privacy policy and terms pages

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -4,8 +4,10 @@ export default async function Page() {
 		<p>Last updated: December 10, 2023</p>
 
 		<h2>1. Introduction</h2>
-		<p>This application is developed by individual entrepreneur Aleksandra Kurganova, committed to maintaining the
-			privacy and security of our users&apos; personal information.</p>
+		<p>This application, Alexenda, developed by individual entrepreneur Aleksandra Kurganova, is currently in open
+			beta testing. By using Alexenda, you acknowledge that there may be errors, the application may occasionally
+			malfunction, and user data stored in the application may inadvertently be lost. We are committed to
+			maintaining the privacy and security of our users&apos; personal information.</p>
 
 		<h2>2. Information Collection and Use</h2>
 		<p>We collect the following types of information about our users:</p>
@@ -13,12 +15,10 @@ export default async function Page() {
 			<li>Email address</li>
 			<li>Profile information (e.g., name, profile picture)</li>
 		</ul>
-		<p>This information is collected through the Google account login process and is used to provide and improve our
-			services.</p>
+		<p>This information is collected through the Google account login process and is used to provide and improve our services.</p>
 
 		<h2>3. Sharing of Information</h2>
-		<p>We do not share personal information with companies, organizations, or individuals outside of our application
-			except in the following cases:</p>
+		<p>We do not share personal information with companies, organizations, or individuals outside of our application except in the following cases:</p>
 		<ul>
 			<li>With your consent</li>
 			<li>For legal reasons</li>
@@ -28,9 +28,8 @@ export default async function Page() {
 		<p>We take steps to protect the security of the information we collect from you.</p>
 
 		<h2>5. Changes to This Privacy Policy</h2>
-		<p>We may change this Privacy Policy from time to time. We will notify you of any changes by posting the new
-			Privacy Policy on this page.</p>
+		<p>We may change this Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.</p>
 
-		<p>Contact Information: <a href="mailto:akurganow@googlegroups.com">akurganow@googlegroups.com</a></p>
+		<p>Contact Information: akurganow@googlegroups.com</p>
 	</>
 }
--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -9,10 +9,13 @@ export default async function Page() {
 		<h2>2. Use of Service</h2>
 		<p>Alexenda, developed by individual entrepreneur Aleksandra Kurganova, is a task management application that
 			helps users create, manage, track tasks, and manage time. Key features include recurring tasks, task lists,
-			subtasks, tags, deadlines, and setting priorities.</p>
+			subtasks, tags, deadlines, and setting priorities. Alexenda is currently in open beta testing. Users
+			acknowledge the possibility of encountering errors, occasional malfunctions, and the potential loss of data
+			stored in the application.</p>
 
 		<h2>3. User Obligations</h2>
-		<p>You agree not to misuse the services or help anyone else do so.</p>
+		<p>You agree not to misuse the services or help anyone else do so, and to be aware of the beta status of the
+			application.</p>
 
 		<h2>4. Privacy and Security</h2>
 		<p>Your privacy and the security of your data are important to us. For more information, please see our Privacy
@@ -21,6 +24,6 @@ export default async function Page() {
 		<h2>5. Modification of Terms</h2>
 		<p>We reserve the right to modify these terms at any time. You should review these terms regularly.</p>
 
-		<p>Contact Information: <a href="mailto:akurganow@googlegroups.com">akurganow@googlegroups.com</a></p>
+		<p>Contact Information: akurganow@googlegroups.com</p>
 	</>
 }
