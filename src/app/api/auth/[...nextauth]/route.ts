c9500ca947a92923b8a864f2cf2e8a6547672b73
Alexander Kurganov <a.kurganow@gmail.com>
2023-12-11 15:55:42 +0400

    fix: Build error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,6 @@
-import NextAuth, { type NextAuthOptions } from 'next-auth'
-import { PrismaClient } from '@prisma/client'
-import { PrismaAdapter } from '@auth/prisma-adapter'
-import GoogleProvider from 'next-auth/providers/google'
+import NextAuth from 'next-auth'
+import { authOptions } from 'app/api/auth/constants'
 
-const prisma = new PrismaClient()
-export const authOptions: NextAuthOptions = {
-	adapter: PrismaAdapter(prisma),
-	providers: [
-		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID!,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-		}),
-	],
-}
 const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
---
+++ src/app/api/auth/constants.ts
@@ -0,0 +1,15 @@
+import type { NextAuthOptions } from 'next-auth'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import GoogleProvider from 'next-auth/providers/google'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+export const authOptions: NextAuthOptions = {
+	adapter: PrismaAdapter(prisma),
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID!,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+		}),
+	],
+}
