ca9c6e240b149f2653bc2fc5acc47bd854662c16
Alexander Kurganov <a.kurganow@gmail.com>
2023-12-11 22:15:42 +0400

    feat: add privacy policy and terms pages

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,36 @@
+export default async function Page() {
+	return <>
+		<h1>Privacy Policy</h1>
+		<p>Last updated: December 10, 2023</p>
+
+		<h2>1. Introduction</h2>
+		<p>This application is developed by individual entrepreneur Aleksandra Kurganova, committed to maintaining the
+			privacy and security of our users&apos; personal information.</p>
+
+		<h2>2. Information Collection and Use</h2>
+		<p>We collect the following types of information about our users:</p>
+		<ul>
+			<li>Email address</li>
+			<li>Profile information (e.g., name, profile picture)</li>
+		</ul>
+		<p>This information is collected through the Google account login process and is used to provide and improve our
+			services.</p>
+
+		<h2>3. Sharing of Information</h2>
+		<p>We do not share personal information with companies, organizations, or individuals outside of our application
+			except in the following cases:</p>
+		<ul>
+			<li>With your consent</li>
+			<li>For legal reasons</li>
+		</ul>
+
+		<h2>4. Security</h2>
+		<p>We take steps to protect the security of the information we collect from you.</p>
+
+		<h2>5. Changes to This Privacy Policy</h2>
+		<p>We may change this Privacy Policy from time to time. We will notify you of any changes by posting the new
+			Privacy Policy on this page.</p>
+
+		<p>Contact Information: <a href="mailto:akurganow@googlegroups.com">akurganow@googlegroups.com</a></p>
+	</>
+}
---
+++ src/app/terms/page.tsx
@@ -0,0 +1,26 @@
+export default async function Page() {
+	return <>
+		<h1>Terms of Service</h1>
+		<p>Last updated: December 10, 2023</p>
+
+		<h2>1. Acceptance of Terms</h2>
+		<p>By using Alexenda, you agree to be bound by these terms and conditions.</p>
+
+		<h2>2. Use of Service</h2>
+		<p>Alexenda, developed by individual entrepreneur Aleksandra Kurganova, is a task management application that
+			helps users create, manage, track tasks, and manage time. Key features include recurring tasks, task lists,
+			subtasks, tags, deadlines, and setting priorities.</p>
+
+		<h2>3. User Obligations</h2>
+		<p>You agree not to misuse the services or help anyone else do so.</p>
+
+		<h2>4. Privacy and Security</h2>
+		<p>Your privacy and the security of your data are important to us. For more information, please see our Privacy
+			Policy.</p>
+
+		<h2>5. Modification of Terms</h2>
+		<p>We reserve the right to modify these terms at any time. You should review these terms regularly.</p>
+
+		<p>Contact Information: <a href="mailto:akurganow@googlegroups.com">akurganow@googlegroups.com</a></p>
+	</>
+}
