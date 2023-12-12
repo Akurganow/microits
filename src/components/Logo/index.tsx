c412f655c7dfca9d61b79f36a222e2184b12e4e1
Alexander Kurganov <a.kurganow@gmail.com>
2023-12-12 17:49:34 +0400

    fix: logo size

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -2,10 +2,12 @@ import Image from 'next/image'
 import st from './styles.module.css'
 import { useTranslation } from 'react-i18next'
 
+const logoSize = 32
+
 export default function Logo() {
 	const { t } = useTranslation()
 
-	return <div className={st.logo} title="Alexenda">
-		<Image src="/logo.svg" alt={t('appName')} width={48} height={48} />
+	return <div className={st.logo} style={{ height: logoSize }} title="Alexenda">
+		<Image src="/logo.svg" alt={t('appName')} width={logoSize} height={logoSize} />
 	</div>
 }
