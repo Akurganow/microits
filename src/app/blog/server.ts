c9180501e8626251a6b3c1437edc40aaa803aec6
Alexander Kurganov <a.kurganow@gmail.com>
2023-12-13 18:22:22 +0400

    fix: export preview client

--- src/app/blog/server.ts
+++ src/app/blog/server.ts
@@ -2,7 +2,6 @@
 import { client } from 'app/contentful'
 import { BlogPostEntry } from 'app/blog/types'
 
-
 export async function getBlogEntries () {
 	const entries = await client.getEntries<BlogPostEntry>({
 		content_type: 'blogPage',
--- src/app/contentful.ts
+++ src/app/contentful.ts
@@ -1,10 +1,11 @@
 import { createClient } from 'contentful'
 
-const proClient = createClient({
+export const proClient = createClient({
 	space: process.env.CF_SPACE_ID as string,
 	accessToken: process.env.CF_DELIVERY_ACCESS_TOKEN as string,
 })
-const previewClient = createClient({
+
+export const previewClient = createClient({
 	space: process.env.CF_SPACE_ID as string,
 	accessToken: process.env.CF_DELIVERY_PREVIEW_TOKEN as string,
 	host: 'preview.contentful.com',
